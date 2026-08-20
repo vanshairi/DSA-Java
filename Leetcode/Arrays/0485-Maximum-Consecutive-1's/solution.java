1a3d3abab5f154ee8264f664a752c4a287fc6908
Vansh Airi <vanshairi0@gmail.com>
2026-08-20 12:47:11 +0530

    Solve maximum consecutive 1's in a binary array.

---
+++ Leetcode/Arrays/0485-Maximum-Consecutive-1's/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int findMaxConsecutiveOnes(int[] nums) {
+    int count = 0;
+    int maxCount = 0;
+
+    for (int i : nums) {
+      if (i == 1) {
+        count++;
+        if (count > maxCount) {
+          maxCount = count;
+        }
+      } else {
+        count = 0;
+      }
+    }
+    return maxCount;
+  }
+}
